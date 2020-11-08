c8fb2799dc4e063f216c1442ff78c987c375ad2b
Samuel McKean <samuel.mckean@gmail.com>
2020-11-08 12:14:04 -0600

    got _updateProjectDOM working

--- src/index.js
+++ src/index.js
@@ -7,30 +7,38 @@ const domModifier = (function() {
   // module which manipulates all the DOM for the application
 
   // get major DOM elements and stores as vars
-  const root = document.getElementById('root');
-  const projectList = document.getElementById('project-list');
-  const todosConatainer = document.getElementById('todos-container');
+  const projectContainer = document.getElementById('projects-container');
+  const todosContainer = document.getElementById('todos-container');
+
+  // initialize project and todos list from app
+  let projectList = app.projects;
+  let currentSelectedProject = projectList[0];  // start with first project as selected
+  let todoList = currentSelectedProject.getTodos();
+
+  const _updatedSelectedProject = function() {
+    // updates which project is the current selected project in DOM
+  }
 
   const _updateProjectsDOM = function() {
     // refreshes the projects container with all projects in the app
-    const elements = [];
-    for (let project of app.projects) {
+    projectContainer.replaceChildren();
+    for (let project of projectList) {
       // create HTML element for each project and push onto array
       const element = document.createElement('div');
       element.innerHTML = project.projectName;
-      elements.push(element);
+      element.classList.add('project');
+      projectContainer.append(element);
     }
-    projectList.replaceChildren(elements);
   }
 
   const _updateTodosDOM = function() {
     // refreshes the projects container with all the todos for the currently selected project
   }
 
-  const addProject = function() {
+  const addProject = function(newProject) {
     // brings up form for new project, adds it to the list, and updates the DOM
     // FIXME: replace with new project from form
-    app.addProject(project);
+    app.addProject(newProject);
     _updateProjectsDOM();
   }
 
@@ -48,6 +56,12 @@ const domModifier = (function() {
     // deletes the todo from that projects todos list and updates DOM
   }
 
+  // update project list in DOM to start
+  _updateProjectsDOM();
+
+  // update todos for selected project in DOM to start
+  _updateTodosDOM();
+
   return {
     addProject,
     removeProject,
@@ -58,7 +72,4 @@ const domModifier = (function() {
 
 // all code below this is for testing
 const project = Project('hello', {name: 'hello'});
-domModifier.addProject();
-for (let project of app.projects) {
-  console.log(project)
-}
+domModifier.addProject(project);
